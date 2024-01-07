e5bc5c3be85606d5ff9c04d5bc273ab1abeb96a7
PERTZ42 <perttu.nurmi+github@gmail.com>
2024-01-07 19:46:22 +0200

    rust

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -5,8 +5,11 @@ require("mason-lspconfig").setup()
 -- After setting up mason-lspconfig you may set up servers via lspconfig
 lspconfig.lua_ls.setup {}
 lspconfig.pylsp.setup {}
-lspconfig.eslint.setup {}
 lspconfig.bashls.setup {}
 lspconfig.gopls.setup {}
--- require("lspconfig").rust_analyzer.setup {}
 
+lspconfig.rust_analyzer.setup {
+    cmd = {
+        "rustup", "run", "stable", "rust-analyzer",
+    }
+}
